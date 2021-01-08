6d116f9cca51681e6285f3bc8510ef41004d5693
Scott Baldwin <sbaldwin621@gmail.com>
2021-01-07 21:42:50 -0500

    Updated template

--- template/src/lib.rs
+++ template/src/lib.rs
@@ -1,7 +1,12 @@
 use std::error::Error;
+use std::fmt::Display;
 use std::fs::{File};
 use std::io::{self, BufRead};
 use std::path::Path;
+
+use config::Config;
+
+pub mod config;
 
 pub fn run(config: Config) -> Result<u32, Box<dyn Error>> {
     let filename = config.filename;
@@ -20,19 +25,17 @@ where P: AsRef<Path>, {
     Ok(io::BufReader::new(file).lines())
 }
 
-pub struct Config {
-    pub filename: String
+#[derive(Debug)]
+pub enum ApplicationError {
+    AnError
 }
 
-impl Config {
-    pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 2 {
-            return Err("not enough arguments");
-        }
-
-        let filename = args[1].clone();
-    
-        Ok(Config { filename })
+impl Display for ApplicationError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        writeln!(f, "{}", match self {
+            AnError => "an error occurred"  
+        })
     }
 }
 
+impl Error for ApplicationError { }
--- template/src/main.rs
+++ template/src/main.rs
@@ -1,7 +1,7 @@
 use std::env;
 use std::process;
 
-use reportrepair::{run, Config};
+use template::{run, config::Config};
 
 fn main() {
     let args: Vec<String> = env::args().collect();

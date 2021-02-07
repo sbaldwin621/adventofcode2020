fcac183340e3bd8147dbaf96a6052439a445d811
Scott Baldwin <scott@Scotts-MacBook-Air.local>
2021-02-07 17:07:33 -0500

    GPuzzle 21-1 complete

--- day21/allergens/src/lib.rs
+++ day21/allergens/src/lib.rs
@@ -7,13 +7,14 @@ use std::path::Path;
 
 use config::Config;
 use food::{Allergen, FoodList, Ingredient};
+use nom::bitvec::index::BitPos;
 use parser::food_list;
 
 pub mod config;
 mod food;
 mod parser;
 
-pub fn run(config: Config) -> Result<u32, Box<dyn Error>> {
+pub fn run(config: Config) -> Result<usize, Box<dyn Error>> {
     let filename = config.filename;
 
     let input = read_to_string(filename)?;
@@ -22,14 +23,19 @@ pub fn run(config: Config) -> Result<u32, Box<dyn Error>> {
 
     // println!("{:?}", foods);
 
-    count_nonallergens(&foods);
+    let allergen_to_ingredient = map_allergens(&foods);
+    let ingredients_with_allergens = allergen_to_ingredient.values().map(|i| i.clone()).collect::<HashSet<_>>();
+    let nonallergens = foods.all_ingredients.difference(&ingredients_with_allergens).map(|i| i.clone()).collect::<HashSet<_>>();
 
-    Ok(0)
+    let mut count = 0;
+    for food in foods.food.iter() {
+        count += food.ingredients.intersection(&nonallergens).count();
+    }
+
+    Ok(count)
 }
 
-fn count_nonallergens(food_list: &FoodList) -> usize {
-    let mut count = 0;
-
+fn map_allergens(food_list: &FoodList) -> HashMap<Allergen, Ingredient> {
     let mut allergen_to_ingredient = HashMap::new();
     let mut assigned_ingredients = HashSet::new();
 
@@ -45,26 +51,42 @@ fn count_nonallergens(food_list: &FoodList) -> usize {
                 ingredient_set = food.ingredients.clone();
             }
 
-            if ingredient_set.len() == 1 {
-                let ingredient = ingredient_set.into_iter().nth(0).unwrap();
+            allergen_to_potential_ingredients.insert(allergen.clone(), ingredient_set);
+        }
+    }
+
+    let mut change_was_made = true;
+    while change_was_made {
+        change_was_made = false;
+
+        for (allergen, ingredients) in allergen_to_potential_ingredients.iter_mut() {
+            for ingredient in assigned_ingredients.iter() {
+                if ingredients.remove(ingredient) {
+                    change_was_made = true;
+                }
+            }
+            
+            if ingredients.len() == 1 {
+                change_was_made = true;
+
+                let ingredient = ingredients.iter().nth(0).unwrap().clone();
+                ingredients.remove(&ingredient);
+    
                 assigned_ingredients.insert(ingredient.clone());
                 allergen_to_ingredient.insert(allergen.clone(), ingredient);
-            } else if ingredient_set.len() > 1 {
-                allergen_to_potential_ingredients.insert(allergen.clone(), ingredient_set);
+            }
+        }
+
+        for allergin in allergen_to_ingredient.keys() {
+            if let Some(_) = allergen_to_potential_ingredients.remove(allergin) {
+                change_was_made = true;
             }
         }
     }
 
-    println!("definite:  {:?}", allergen_to_ingredient);
-    println!("potential: {:?}", allergen_to_potential_ingredients);
-
-    for (allergen, mut ingredients) in allergen_to_potential_ingredients.iter_mut() {
-        for ingredient in assigned_ingredients.iter() {
-            ingredients.remove(ingredient);
-        }
-    }
-
-    count
+    println!("{:?}", allergen_to_ingredient);
+    
+    allergen_to_ingredient
 }
 
 #[derive(Debug)]
